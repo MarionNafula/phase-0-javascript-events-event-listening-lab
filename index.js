5993005f4569f955b432f0f8fc58ac5b6eaac134
MarionNafula <wekesamarion100@gmail.com>
2022-12-10 23:56:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
+//function addingEventListener() {
+    function addingEventListener() {
 
-// Your code here
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-
-    if (left> 0) {
-        dodger.style.left= `${left -1}px`;
+        const  input = document.getElementById('button');
+    function clickAlert() {
+        alert('I was clicked!');
     }
-}
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
+    input.addEventListener('click', clickAlert);
+    
     }
-});
-
-document.addEventListener("keydown", function (e) {
-    if(e.key === "ArrowLeft")
-    moveDodgerLeft();
-});
-function moveDodgerRight(){
-    const rightNumbers = dodger.style.left.replace("px", "");
-    const right = parseInt(rightNumbers, 10);
-
-    if (right >0) {
-        dodger.style.left= `${right + 1}px`;
-    }
-}
-document.addEventListener("keydown",function (e) {
-    if(e.key=== "ArrowLeft") {
-        moveDodgerRight();
-    }
-});
  
 
 
